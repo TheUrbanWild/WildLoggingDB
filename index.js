325296de481e8ea2f220991f7f563ea503f8154f
Coops <coopsatwork@googlemail.com>
2019-01-14 15:22:15 +0000

    added check for optional environment variables which override the the schema, domain and port setting for the SwaggerUI in the swagger.yaml file. This is so we can deploy to any server location without changing the source code.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var app = require('connect')();
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var database = require('./service/database');
-var serverPort = process.env.PORT || 8080;
+
 var dbUrl = process.env.DATABASE_URL;
 
 // URL provided by the Auth0 authentication PaaS
@@ -53,6 +53,8 @@ var writeAuthClientConfig = function (config) {
   fs.writeFileSync('./swagger_spwa/authproviderconfig.js', authenticationClientContent);
 }
 
+
+
 // Cross Origin Requests - must have this, as we are an API.
 // Without it, browsers running SPWAs from domains different to ours (e.g. github pages)
 // will reject HTTP requests during pre-flight check.
@@ -87,6 +89,68 @@ for (var secDef in secDefs) {
 }
 
 
+var getSwaggerUIConfig = function(){
+  var result = {};
+
+  result.scheme = process.env.API_SCHEME;
+  result.domain = process.env.API_DOMAIN;
+  result.port = process.env.API_PORT;
+  result.existingPort = process.env.PORT; // assigned by Heroku if deployed.
+  
+  return result;
+
+}
+
+var writeSwaggerUIConfig = function(swaggerDoc, env){
+
+  var doc = {};
+  doc.scheme = swaggerDoc.schemes[0];  //WILL THROW IF SCHEMES NOT DEFINED IN DOC
+  doc.domain = swaggerDoc.host.split(':')[0];  //WILL THROW IF HOST NOT DEFINED IN DOC 
+  doc.port = swaggerDoc.host.split(':')[1];  //WILL THROW IF PORT NOT DEFINED IN DOC  
+  
+  if (env.existingPort){
+    console.log("remote deployment has already defined port");
+    if(env.port){
+      
+      doc.port = env.port; // override (useful for consistency, or if the remote service is not heroku, and listening on a different port.)
+      console.log("external facing port env variable is set. Updating swagger.yaml with this value: %s", doc.port);
+    }else{
+      doc.port = 443; // override the setting with Heroku's default external facing port
+      console.log("external facing port env variable is unset. Updating swagger.yaml with default value: %s", doc.port);
+    }
+  }else{
+    console.log("local deployment.");
+    if(env.port){
+        doc.port = env.port; //override (useful if you have lots of servers running on local host)
+        console.log("overriding swagger.yaml port to env variable: %s", doc.port);
+    }else{
+        env.port = doc.port;
+        console.log("env varable not defined for port. Setting to default from swagger.yaml: %s ", env.port );
+    }
+  }
+  if(env.domain){
+    doc.domain = env.domain; //override (useful if you want to deploy to a different server than specified in the yaml)
+    console.log("overriding swagger.yaml domain to env variable: %s", doc.domain);
+  }
+  if(env.scheme){
+    doc.scheme = env.scheme; // override (useful if you want to deploy to a different comms scheme than that defined in the yaml.
+    console.log("overriding swagger.yaml scheme to env variable: %s", doc.scheme);
+  }
+  
+  var hostAddrPort = doc.domain + ":" + doc.port;
+  var schemes = [doc.scheme];
+
+  swaggerDoc.host = hostAddrPort;
+  swaggerDoc.schemes = schemes;
+  
+  return swaggerDoc;
+}
+
+var swaggerUIConfig = getSwaggerUIConfig();
+swaggerDoc = writeSwaggerUIConfig(swaggerDoc, swaggerUIConfig);
+var serverPort = swaggerUIConfig.existingPort || swaggerUIConfig.port;
+
+
 // Initialize the Swagger middleware
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 
@@ -111,8 +175,10 @@ swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 
   // Start the server
   http.createServer(app).listen(serverPort, function () {
-    console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
-    console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+    console.log('Your server available at: %s://%s', swaggerDoc.schemes[0], swaggerDoc.host);
+    console.log('The internal port assigned to your server  is: %d', serverPort );
+    console.log('Your swaggerUI is available at: %s://%s/docs' , swaggerDoc.schemes[0], swaggerDoc.host);
+  
   });
 
 });
